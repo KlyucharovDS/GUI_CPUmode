c5ef8c4de7aa7e59ed9b1cc137b8adc09b918c1d
dmitry <ris4286@gmail.com>
2023-09-17 18:24:04 +0400

    added gui view

--- GUI_CPUmode.py
+++ GUI_CPUmode.py
@@ -1,15 +1,11 @@
 #!/bin/python3
 
-from tkinter import *
-from tkinter.messagebox import *
 import argparse
-from pprint import pprint
 from cpufreq import cpuFreq
-from time import sleep
 import time
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QSpinBox, QSlider, QComboBox, QLabel
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
 true = True
 false = False
 
@@ -101,59 +97,191 @@ class CPU:
     @staticmethod
     def get_min_freq():
         return CPU.__fmin
+# Controller
+class GUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Widget()
+        self.ui.setupUi(self)
 
-class GUI:
+        # registration of elements events
+        self.ui.applyBtn.clicked.connect(self.applyBtn_clicked)
 
 
-    def __init__(self, title:str, freq_values:list):
-        # Создаем экземпляр приложения
-        self.__app = QApplication(sys.argv)
+    # ---------------events methods-------------------
+    def applyBtn_clicked(self):
+        pass
 
-        # Создаем главное окно (QWidget)
-        self.__window = QWidget()
-        self.__window.setWindowTitle(title)
-        self.__window.setGeometry(100, 100, 400, 500)
-        self.__window.setFixedHeight(500)
-        self.__window.setFixedWidth(400)
 
-        # добавляем элементы управления
-        self.__closeBtn = QPushButton('Close', self.__window)
-        self.__closeBtn.setGeometry(250, 145, 90, 30)
-        self.__applyBtn = QPushButton('Apply', self.__window)
-        self.__applyBtn.setGeometry(50, 145, 90, 30)
-        # QSlider + QLaber
-        self.__freq = QSlider(Qt.Horizontal, self.__window)
-        self.__freq.setSingleStep(100000)
-        freq_values.sort()
-        self.__freq.setMinimum(freq_values[0])
-        self.__freq.setMaximum(freq_values[-1])
-        x = 50
-        y = 50
-        w = 300
-        h = 10
-        self.__freq.setGeometry(x,y,w,h)
-        self.__freq_label0 = QLabel(self.__window)
-        self.__freq_label0.setGeometry(x+(w-x)/2,y-25,120,20)
-        self.__freq_label0.setText(str(self.__freq.value()))
-        self.__freq_label0.show()
-        self.__freq.show()
 
-        # Подключаем обработчик события к кнопке
-        self.__closeBtn.clicked.connect(self.__on_button_click)
-        self.__freq.valueChanged.connect(self.__move)
 
-        # Отображаем окно
-        self.__window.show()
+# View
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        Widget.setObjectName("Widget")
+        Widget.resize(631, 499)
+        Widget.setFixedSize(631, 499)
 
-        # Запускаем главный цикл приложения
-        self.__exit()
+        self.tabWidget = QtWidgets.QTabWidget(Widget)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 631, 521))
+        self.tabWidget.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
+        self.tabWidget.setObjectName("tabWidget")
 
-    # Создаем обработчик события для кнопки
-    def __on_button_click(self):
-        self.__exit()
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
 
-    def __move(self):
-        self.__freq_label0.setText(str(self.__freq.value()))
+        self.governorGroupBox = QtWidgets.QGroupBox(self.tab)
+        self.governorGroupBox.setGeometry(QtCore.QRect(10, 10, 371, 281))
+        self.governorGroupBox.setObjectName("governorGroupBox")
+
+        self.listView = QtWidgets.QListView(self.governorGroupBox)
+        self.listView.setGeometry(QtCore.QRect(10, 80, 251, 192))
+        self.listView.setObjectName("listView")
+
+        self.govLbl_2 = QtWidgets.QLabel(self.governorGroupBox)
+        self.govLbl_2.setGeometry(QtCore.QRect(10, 60, 151, 19))
+        self.govLbl_2.setObjectName("govLbl_2")
+
+        self.applyGovBtn = QtWidgets.QPushButton(self.governorGroupBox)
+        self.applyGovBtn.setGeometry(QtCore.QRect(270, 80, 94, 27))
+        self.applyGovBtn.setObjectName("applyGovBtn")
+
+        self.resetGovBtn = QtWidgets.QPushButton(self.governorGroupBox)
+        self.resetGovBtn.setGeometry(QtCore.QRect(270, 120, 94, 27))
+        self.resetGovBtn.setObjectName("resetGovBtn")
+
+        self.govLbl_1 = QtWidgets.QLabel(self.governorGroupBox)
+        self.govLbl_1.setGeometry(QtCore.QRect(10, 30, 131, 19))
+        self.govLbl_1.setObjectName("govLbl_1")
+
+        self.currGovLbl = QtWidgets.QLabel(self.governorGroupBox)
+        self.currGovLbl.setGeometry(QtCore.QRect(140, 29, 131, 20))
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(30)
+        sizePolicy.setHeightForWidth(self.currGovLbl.sizePolicy().hasHeightForWidth())
+
+        self.currGovLbl.setSizePolicy(sizePolicy)
+        self.currGovLbl.setText("")
+        self.currGovLbl.setObjectName("currGovLbl")
+
+        self.freqGroupBox = QtWidgets.QGroupBox(self.tab)
+        self.freqGroupBox.setGeometry(QtCore.QRect(10, 300, 611, 121))
+        self.freqGroupBox.setObjectName("freqGroupBox")
+        self.freqLbl = QtWidgets.QLabel(self.freqGroupBox)
+        self.freqLbl.setGeometry(QtCore.QRect(180, 60, 181, 19))
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(100)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.freqLbl.sizePolicy().hasHeightForWidth())
+
+        self.freqLbl.setSizePolicy(sizePolicy)
+        self.freqLbl.setObjectName("freqLbl")
+        self.freqHorizontalSlider = QtWidgets.QSlider(self.freqGroupBox)
+        self.freqHorizontalSlider.setGeometry(QtCore.QRect(10, 80, 481, 16))
+        self.freqHorizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.freqHorizontalSlider.setObjectName("freqHorizontalSlider")
+
+        self.applyFreqBtn = QtWidgets.QPushButton(self.freqGroupBox)
+        self.applyFreqBtn.setGeometry(QtCore.QRect(510, 40, 94, 27))
+        self.applyFreqBtn.setObjectName("applyFreqBtn")
+
+        self.resetFreqBtn = QtWidgets.QPushButton(self.freqGroupBox)
+        self.resetFreqBtn.setGeometry(QtCore.QRect(510, 80, 94, 27))
+        self.resetFreqBtn.setObjectName("resetFreqBtn")
+
+        self.closeBtn = QtWidgets.QPushButton(self.tab)
+        self.closeBtn.setGeometry(QtCore.QRect(520, 430, 94, 27))
+        self.closeBtn.setObjectName("closeBtn")
+
+        self.applyBtn = QtWidgets.QPushButton(self.tab)
+        self.applyBtn.setGeometry(QtCore.QRect(10, 430, 94, 27))
+        self.applyBtn.setObjectName("applyBtn")
+
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setGeometry(QtCore.QRect(280, 430, 94, 27))
+        self.pushButton.setObjectName("pushButton")
+
+        self.periodGroupBox = QtWidgets.QGroupBox(self.tab)
+        self.periodGroupBox.setGeometry(QtCore.QRect(390, 10, 231, 151))
+        self.periodGroupBox.setObjectName("periodGroupBox")
+
+        self.enPeriodChkBox = QtWidgets.QCheckBox(self.periodGroupBox)
+        self.enPeriodChkBox.setGeometry(QtCore.QRect(10, 30, 171, 25))
+        self.enPeriodChkBox.setObjectName("enPeriodChkBox")
+
+        self.periodLbl = QtWidgets.QLabel(self.periodGroupBox)
+        self.periodLbl.setEnabled(False)
+        self.periodLbl.setGeometry(QtCore.QRect(10, 70, 51, 19))
+        self.periodLbl.setObjectName("periodLbl")
+
+        self.timeEdit = QtWidgets.QTimeEdit(self.periodGroupBox)
+        self.timeEdit.setEnabled(False)
+        self.timeEdit.setGeometry(QtCore.QRect(70, 70, 118, 28))
+        self.timeEdit.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
+        self.timeEdit.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedKingdom))
+        self.timeEdit.setCurrentSection(QtWidgets.QDateTimeEdit.HourSection)
+        self.timeEdit.setCalendarPopup(False)
+        self.timeEdit.setObjectName("timeEdit")
+
+        self.applyPeriodBtn = QtWidgets.QPushButton(self.periodGroupBox)
+        self.applyPeriodBtn.setEnabled(False)
+        self.applyPeriodBtn.setGeometry(QtCore.QRect(20, 110, 94, 27))
+        self.applyPeriodBtn.setObjectName("applyPeriodBtn")
+
+        self.resetPeriodBtn = QtWidgets.QPushButton(self.periodGroupBox)
+        self.resetPeriodBtn.setEnabled(False)
+        self.resetPeriodBtn.setGeometry(QtCore.QRect(130, 110, 94, 27))
+        self.resetPeriodBtn.setObjectName("resetPeriodBtn")
+
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+
+        self.logPlainTextEdit = QtWidgets.QPlainTextEdit(self.tab_2)
+        logPlainTextEdit_size = QtCore.QRect(10, 20, 607, 436)
+        self.logPlainTextEdit.setGeometry(logPlainTextEdit_size)
+        self.logPlainTextEdit.setObjectName("logPlainTextEdit")
+        self.logPlainTextEdit.setUpdatesEnabled(true)
+        self.logPlainTextEdit.setEnabled(false)
+
+        self.label = QtWidgets.QLabel(self.tab_2)
+        self.label.setGeometry(QtCore.QRect(10, 0, 72, 19))
+        self.label.setObjectName("label")
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.retranslateUi(Widget,'GUI_CPUmode')
+        self.tabWidget.setCurrentIndex(0)
+
+        QtCore.QMetaObject.connectSlotsByName(Widget)
+
+    def retranslateUi(self, Widget,title_name:str):
+        _translate = QtCore.QCoreApplication.translate
+        Widget.setWindowTitle(title_name)
+        self.governorGroupBox.setTitle(_translate("Widget", "Setting governor"))
+        self.govLbl_2.setText(_translate("Widget", "Available governors:"))
+        self.applyGovBtn.setText(_translate("Widget", "Apply"))
+        self.resetGovBtn.setText(_translate("Widget", "Cancel"))
+        self.govLbl_1.setText(_translate("Widget", "Current governor:"))
+        self.freqGroupBox.setTitle(_translate("Widget", "Frequency"))
+        self.freqLbl.setText(_translate("Widget", "5 000 000 KHz (5.0 GHz)"))
+        self.applyFreqBtn.setText(_translate("Widget", "Apply"))
+        self.resetFreqBtn.setText(_translate("Widget", "Cancel"))
+        self.closeBtn.setText(_translate("Widget", "Close"))
+        self.applyBtn.setText(_translate("Widget", "Apply"))
+        self.pushButton.setText(_translate("Widget", "Cancel"))
+        self.periodGroupBox.setTitle(_translate("Widget", "Period"))
+        self.enPeriodChkBox.setText(_translate("Widget", "Enable period install"))
+        self.periodLbl.setText(_translate("Widget", "Period"))
+        self.timeEdit.setDisplayFormat(_translate("Widget", "HH:mm:ss"))
+        self.applyPeriodBtn.setText(_translate("Widget", "Apply"))
+        self.resetPeriodBtn.setText(_translate("Widget", "Cancel"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Widget", "Settings"))
+        self.label.setText(_translate("Widget", "Log:"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("Widget", "Log"))
+
 
 
     def __exit(self,status=None):
@@ -282,12 +410,9 @@ if "__main__" == __name__:
 
     # GUI
     if len(sys.argv) == 1:
-        gui=GUI(title='CPU mode',freq_values=CPU.get_available_freq())
+        app = QApplication(sys.argv)
+        main_window = GUI()
+        main_window.show()
+        sys.exit(app.exec_())
 
-
-
-
-
-
-
-
+        #gui=GUI(title='CPU
